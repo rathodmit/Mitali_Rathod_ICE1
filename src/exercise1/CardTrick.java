bb3a249ab1c209bbded8f90b45ff7fe6286253d3
rathomit <rathomit@sheridancollege.ca>
2023-01-25 15:31:33 -0500

    In Class 1 branched code

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,5 +1,7 @@
 package exercise1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
@@ -13,16 +15,33 @@ public class CardTrick {
     public static void main(String[] args) {
         
         Card[] hand = new Card[7];
+        
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
-            //       Don't worry about duplicates at this point
+            card.setValue((int)(1+Math.random()*13));
+            card.setSuit(Card.SUITS[(int)(0+Math.random()*3)]);
+    
+            hand[i]=card;
+            System.out.println(hand[i].getSuit() + hand[i].getValue());
         }
 
+        Scanner kb = new Scanner(System.in);
+        
+        System.out.println("Please enter the value of your card between 1 to 13");
+        int value =kb.nextInt();
+        System.out.println("Please enter the value of your suit"); 
+        String suit = kb.next();
+        for(int i =0;i<hand.length;i++)
+        {
+         if(value==hand[i].getValue() && suit.equalsIgnoreCase(hand[i].getSuit()))
+          
+        {
+            printInfo();
+        }
+        }
+    
+        
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here. 
         // Hint: You can ask for values 1 to 10, and then
@@ -45,12 +64,12 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Mitali, but you can call me Mini");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- I want to become an IT Technician");
+        System.out.println("-- ");
 	System.out.println();	
 
         System.out.println("My hobbies:");
